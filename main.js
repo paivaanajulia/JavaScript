4c734d00bdc03fde3096bf8e365ea4e3299c5507
paivaanajulia <116007247+paivaanajulia@users.noreply.github.com>
2024-03-04 08:18:46 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 const botoes = document.querySelectorAll (".botao");
-console.log(botoes)
+const textos = document.querySelectorAll("aba-conteudo");
 
-for(let i=0;i <botoes.length;i++){ botoes[i].onclick = function(){
+for(let i=0;i <botoes.length;i++){
+     botoes[i].onclick = function(){
 
-    for(let j=0;j <botoes.length;j++) { botoes[j].classList.remove("ativo");
-    }
- botoes[i].classList.add("ativo");
-}
-}
+            for(let j=0;j <botoes.length;j++) {
+                 botoes[j].classList.remove("ativo");
+                 textos[j].classList.remove("ativo")
+      }
+          botoes[i].classList.add("ativo");
+          textos[i].classList.add("ativo");
+   }
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024")
+contadores[0].textContent = "Contagem regressiva";
+
+  }
