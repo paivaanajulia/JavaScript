cfa5ce0da4bbc1357d207a9ae3ae14981df2cf56
paivaanajulia <116007247+paivaanajulia@users.noreply.github.com>
2024-03-26 09:52:18 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -17,14 +17,23 @@ const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
 const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
 const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
-contadores[1].textContent = calculaTempo(tempoObjetivo2);
+const tempo = [tempoObjetivo1,tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
 function calculaTempo(tempoObjetivo) {
      let tempoAtual = new Date();
      let tempoFinal = tempoObjetivo - tempoAtual;
-     let segundos = 
-}
+     let segundos = Math.floor(tempoFinal / 1000);
+     let minutos = Math.floor(segundos / 60);
+     let horas = Math.floor(minutos / 60);
+     let dias = Math.floor(horas / 24);
 
+     segundos %= 60;
+     minutos%= 60;
+     horas %=;
 
+     if (tempoFinal > 0) {
+          return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundo";
+          } else {
+          return[0,0,0,0]       
+          }
   }
